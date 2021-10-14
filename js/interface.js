9fb612cdda117fc21e41cd1d0a1d608cbecacd12
Carlos Toxtli <ctoxtli@gmail.com>
2021-10-13 20:59:19 -0400

    Interface improved

--- js/interface.js
+++ js/interface.js
@@ -1,3 +1,115 @@
+var maxTasks = 4;
+var globalTasks = null;
+
+Object.flatten = function(data) {
+    var result = {};
+    function recurse (cur, prop) {
+        if (Object(cur) !== cur) {
+            result[prop] = cur;
+        } else if (Array.isArray(cur)) {
+             for(var i=0, l=cur.length; i<l; i++)
+                 recurse(cur[i], prop + "[" + i + "]");
+            if (l == 0)
+                result[prop] = [];
+        } else {
+            var isEmpty = true;
+            for (var p in cur) {
+                isEmpty = false;
+                recurse(cur[p], prop ? prop+"."+p : p);
+            }
+            if (isEmpty && prop)
+                result[prop] = {};
+        }
+    }
+    recurse(data, "");
+    return result;
+};
+
+Object.unflatten = function(data) {
+    "use strict";
+    if (Object(data) !== data || Array.isArray(data))
+        return data;
+    var regex = /\.?([^.\[\]]+)|\[(\d+)\]/g,
+        resultholder = {};
+    for (var p in data) {
+        var cur = resultholder,
+            prop = "",
+            m;
+        while (m = regex.exec(p)) {
+            cur = cur[prop] || (cur[prop] = (m[2] ? [] : {}));
+            prop = m[2] || m[1];
+        }
+        cur[prop] = data[p];
+    }
+    return resultholder[""] || resultholder;
+};
+
+function formatNumber(number) {
+    if (number == null)
+        return " -- ";
+    return '$' + parseFloat(number).toFixed(2);
+}
+
+function getTaskType(task) {
+    var types = {
+        "yt_project_class__snippet__classification": "CLS",
+        "yt_project_class__snippet__user_content": "USR",
+        "yt_project_class__snippet__web_searching": "WEB",
+        "yt_project_class__snippet__field_task": "FLD",
+        "yt_project_class__snippet__moderation": "MOD",
+        "yt_project_class__snippet__unknown": "UNK",
+        "yt_project_class__snippet__relevance": "REL"
+    };
+    var meta = task.projectMetaInfo.experimentMeta;
+    var taskType = "NON";
+    for (var typeTask in types) {
+        if (meta.hasOwnProperty(typeTask)) {
+            taskType = types[typeTask];
+            break;
+        }
+    }
+    return taskType;
+}
+
+function formatTime(seconds) {
+    if (seconds < 60)
+        return seconds + 's';
+    return parseInt(seconds/60) + 'm';
+}
+
+function normalize(obj) {
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            if (typeof obj[key] === 'object') {
+                if (key === 'data') {
+                    // check if data object also has a data property
+                    if (obj[key].hasOwnProperty('data')) {
+                        // double recursion
+                        normalize(obj[key]);
+                        normalize(obj);
+                    }
+                    else {
+                        // copy all values to the parent
+                        // (only if they don't exist in the parent yet)
+                        for (var subKey in obj[key]) {
+                            if (obj[key].hasOwnProperty(subKey)
+                            &&  !obj.hasOwnProperty(subKey)) {
+                                obj[subKey] = obj[key][subKey];
+                            }
+                        }
+                        // remove the data key
+                        delete obj[key];
+                    }
+                }
+                else {
+                    // recursion
+                    normalize(obj[key]);
+                }
+            }
+        }
+    }
+}
+
 function drawInterface() {
     
     $("#header > header > div > div:nth-child(3)").prepend(`
@@ -13,59 +125,19 @@ function drawInterface() {
     $('#alertDiv').append(`
         <div id="alertPopup" class="popup popup_show_bottom-right user-switcher__popup popup_visible">
             <span class="popup__content">
-                <div id="divTasks" style="width:300px;">
+                <div id="divTasks" class="gig-popup-tasks">
                     <div class="user-menu">
                         <div class="user-menu-item">
-                            <span style="float:left;">
+                            <span class="gig-popup-left">
                                 Tasks
                             </span>
-                            <span id="settButton" style="float:right;">
+                            <span id="settButton" class="gig-popup-right">
                                 <svg width="16" height="16" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve"><g><g><path d="M63.8,28.3l-0.2-1.5L55,25.3c-0.2-0.9-0.5-1.7-0.9-2.5c-0.3-0.8-0.7-1.6-1.2-2.4l5-7.1l-0.9-1.2c-1.5-1.9-3.3-3.7-5.2-5.2L50.7,6l-7.1,5c-1.6-0.9-3.2-1.5-4.9-2l-1.5-8.6l-1.5-0.2c-2.5-0.3-5-0.3-7.4,0l-1.5,0.2L25.3,9c-0.9,0.2-1.7,0.5-2.5,0.9c-0.8,0.3-1.6,0.7-2.4,1.2l-7.1-5l-1.2,0.9c-1.9,1.5-3.7,3.3-5.2,5.2L6,13.3l5,7.1c-0.9,1.6-1.5,3.2-2,4.9l-8.6,1.5l-0.2,1.5c-0.3,2.5-0.3,5,0,7.4l0.2,1.5L9,38.7c0.2,0.9,0.5,1.7,0.9,2.5c0.3,0.8,0.7,1.6,1.2,2.4l-5,7.1l0.9,1.2c1.5,1.9,3.3,3.7,5.2,5.2l1.2,0.9l7.1-5c1.6,0.9,3.2,1.5,4.9,2l1.5,8.6l1.5,0.2c1.2,0.1,2.5,0.2,3.7,0.2s2.5-0.1,3.7-0.2l1.5-0.2l1.5-8.6c0.9-0.2,1.7-0.5,2.5-0.9c0.8-0.3,1.6-0.7,2.4-1.2l7.1,5l1.2-0.9c1.9-1.5,3.7-3.3,5.2-5.2l0.9-1.2l-5-7.1c0.9-1.6,1.5-3.2,2-4.9l8.6-1.5l0.2-1.5C64.1,33.3,64.1,30.8,63.8,28.3z M32,44c-6.6,0-12-5.4-12-12s5.4-12,12-12s12,5.4,12,12S38.6,44,32,44z"/></g></g></svg>
                             </span>
                         </div>
                         <div class="user-menu__separator"></div>
                         <div id="taskList" class="user-menu-item">
-                            <table style="width:100%;">
-                                <tr>
-                                    <td>
-                                        <div>
-                                            Title 1
-                                        </div>
-                                        <div>
-                                            Description 1
-                                        </div>
-                                    </td>
-                                    <td>
-                                    <input id="task1" type="checkbox">
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <div>
-                                            Title 2
-                                        </div>
-                                        <div>
-                                            Description 2
-                                        </div>
-                                    </td>
-                                    <td>
-                                    <input id="task2" type="checkbox">
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <div>
-                                            Title 3
-                                        </div>
-                                        <div>
-                                            Description 3
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <input id="task3" type="checkbox">
-                                    </td>
-                                </tr>
-                            </table>
+                           
                         </div>
                         <div class="user-menu__separator"></div>
                         <div class="user-menu__footer">
@@ -82,13 +154,13 @@ function drawInterface() {
                         </div>
                     </div>
                 </div>
-                <div id="divSettings" style="display:none;width:200px;">
+                <div id="divSettings" class="git-popup-sett">
                     <div class="user-menu">
                         <div class="user-menu-item">
-                            <span style="float:left;">
+                            <span class="gig-popup-left">
                                 Settings
                             </span>
-                            <span id="backButton" style="float:right;">
+                            <span id="backButton" class="gig-popup-right">
                                 <svg width="16" height="16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><g><g><g><g><path d="M500,10C229.4,10,10,229.4,10,500c0,270.7,219.4,490,490,490c270.7,0,490-219.3,490-490C990,229.4,770.7,10,500,10z M604.5,688.9c12.7,12.7,12.7,33.5,0,46.2c-12.7,12.7-33.5,12.7-46.2,0L346.5,523.2c-6.4-6.4-9.5-14.8-9.5-23.2c0-8.4,3.1-16.8,9.5-23.2l211.9-211.9c12.7-12.7,33.5-12.7,46.2,0c12.7,12.7,12.7,33.5,0,46.2L415.7,500L604.5,688.9z"/></g></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></g></svg>
                             </span>
                         </div>
@@ -178,11 +250,23 @@ function getTasks() {
     });
 }
 
+function augmentData(tasks) {
+    for (var task of tasks) {
+        task.taskType = getTaskType(task);
+    }
+}
+
 function populateTasks(tasks) {
-    var maxTasks = 3;
     var numTask = 0;
     var html = '<table>';
     var count = 0;
+    augmentData(tasks);
+    tasks.sort((a, b) => (a.pools[0].reward < b.pools[0].reward) ? 
+        1 : (a.pools[0].reward === b.pools[0].reward) ? 
+        ((a.projectStats.averageSubmitTimeSec > b.projectStats.averageSubmitTimeSec) ? 
+        1 : -1) : -1 );
+    globalTasks = tasks;
+    var flatTasks = tasks.map(x => Object.flatten(x));
     for (var task of tasks) {
         if (task.availability.available) {
             if (!task.trainingDetails.training) {
@@ -190,22 +274,73 @@ function populateTasks(tasks) {
                     <tr>
                         <td>
                             <div class="gig-rounded">
-                                <div style="display:inline-block;width:200px;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">
-                                    ${task.title}
+                                <div class="git-label-desc" title="${task.description}">
+                                    <a class="links" href="https://toloka.yandex.com/task/${task.pools[0].id}?refUuid=${task.refUuid}" target="_blank">${task.title}</a>
                                 </div>
-                                <span style="width:50px;">
-                                    ${task.pools[0].reward}
-                                </span>
-                                <span style="width:50px;">
-                                    ${task.projectStats.averageSubmitTimeSec}
-                                </span>
-                            </div>
-                            <div style="width:100px;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">
-                                ${task.description}
+                                <div>
+                                    <!--
+                                    <span class="git-label-field">
+                                        <div class="git-label-top">
+                                            ${task.taskType}
+                                        </div>
+                                        <div class="git-label-sub">
+                                            type
+                                        </div>
+                                    </span>
+                                    -->
+                                    <span class="git-label-field">
+                                        <div class="git-label-top git-label-reward">
+                                            ${formatNumber(task.pools[0].reward)}
+                                        </div>
+                                        <div class="git-label-sub">
+                                            per task
+                                        </div>
+                                    </span>
+                                    <span class="git-label-field">
+                                        <div class="git-label-top">
+                                            ${formatNumber(task.projectStats.moneyMax3)}
+                                        </div>
+                                        <div class="git-label-sub">
+                                            max pay
+                                        </div>
+                                    </span>
+                                    <span class="git-label-field">
+                                        <div class="git-label-top">
+                                            ${formatTime(task.projectStats.averageSubmitTimeSec)}
+                                        </div>
+                                        <div class="git-label-sub">
+                                            time
+                                        </div>
+                                    </span>
+                                    <span class="git-label-field">
+                                        <div class="git-label-top">
+                                            ${formatTime(task.pools[0].assignmentMaxDurationSeconds)}
+                                        </div>
+                                        <div class="git-label-sub">
+                                            max time
+                                        </div>
+                                    </span>
+                                    <span class="git-label-field">
+                                        <div class="git-label-top">
+                                            ${task.projectStats.acceptanceRate}%
+                                        </div>
+                                        <div class="git-label-sub">
+                                            acceptance
+                                        </div>
+                                    </span>
+                                    <span class="git-label-mid">
+                                        <div class="git-label-top git-label-req" title="${task.requesterInfo.name.EN}">
+                                            ${task.requesterInfo.name.EN}
+                                        </div>
+                                        <div class="git-label-sub">
+                                            requester
+                                        </div>
+                                    </span>
+                                </div>
                             </div>
                         </td>
-                        <td>
-                            <input type="checkbox" id="field${count}">
+                        <td class="gig-centered">
+                            <input type="checkbox" class="gig-checkbox" id="field${count}">
                         </td>
                     </tr>
                 `;
